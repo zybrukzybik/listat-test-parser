b123b4038b075831b154568ed10d6705efa92b2b
OZ <zybrukzybik997@gmail.com>
2021-03-01 20:10:07 +0200

    add isHaveExpressionsArr and fix || to && in assert body

--- controllers/data.js
+++ controllers/data.js
@@ -1,6 +1,6 @@
 const assert = require('assert')
 
-const {isArr, isEmptyArr, isObj, isEmptyObj, isArrWithOneEmptyEl} = require('../utils/validation')
+const {isArr, isEmptyArr, isObj, isEmptyObj, isHaveExpressionsArr, isArrWithOneEmptyEl} = require('../utils/validation')
 const writeResults = require('../utils/writeResults')
 const {responseOk} = require('../utils/responses')
 const parser = require('../utils/parser')
@@ -8,16 +8,16 @@ const parser = require('../utils/parser')
 async function data(ctx) {
     const body = ctx.request.body
 
-    assert(isObj(body) || !isEmptyObj(body), 'invalid')   //  check obj || empty obj
+    assert(isObj(body) && !isEmptyObj(body) && isHaveExpressionsArr(body), 'invalid body')   //  check obj || empty obj
 
     const expr = body.expressions
 
-    assert(isArr(expr) && !isEmptyArr(expr) && !isArrWithOneEmptyEl(expr), 'invalid')     //  check arr
+    assert(isArr(expr) && !isEmptyArr(expr) && !isArrWithOneEmptyEl(expr), 'invalid array')     //  check arr
 
     const results = parser(expr)
 
     await writeResults(results.join('\n'))
-    await responseOk()
+    await responseOk(ctx)
 }
 
 module.exports = data
